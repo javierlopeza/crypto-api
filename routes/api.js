b8779e78906acffa1dc519e7e5c255a54828c4e5
Javier López <jilopez8@uc.cl>
2017-12-11 01:48:21 -0300

    Add Cryptomarket ETH

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 router.get('/eth', function(req, res) {
-  if ('eth') {
-    res.json({"success": true});
-  } else {
-    return res.status(403).send({success: false, msg: 'Unauthorized.'});
-  }
+
+  axios.get('https://www.cryptomkt.com/api/ethclp/1.json')
+  .then(function (response) {
+    const bid = parseInt(response.data['data']['prices_bid']['values'][0]['close_price']);
+    const ask = parseInt(response.data['data']['prices_ask']['values'][0]['close_price']);
+    res.json({
+      success: true,
+      data: {
+        bid,
+        ask
+      }
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+    return res.status(500).send({success: false, msg: 'Third Party API error.'});
+  });
+
 });
 module.exports = router;
