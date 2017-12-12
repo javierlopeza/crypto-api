6f796fd62673dd4ce4c1b6a5c0cc1f2851621e50
Javier López <jilopez8@uc.cl>
2017-12-12 02:02:45 -0300

    Interval to retrieve ETH in CryptoMarket

--- jobs/index.js
+++ jobs/index.js
@@ -3,12 +3,12 @@ var axios = require('axios');
 // ETH (CryptoMarket)
 setInterval(() => {
     axios.get('https://www.cryptomkt.com/api/ethclp/1.json')
-    .then(function (response) {
+    .then((response) => {
       const bid = parseInt(response.data['data']['prices_bid']['values'][0]['close_price']);
       const ask = parseInt(response.data['data']['prices_ask']['values'][0]['close_price']);
       console.log({bid, ask});
     })
-    .catch(function (error) {
+    .catch((error) => {
     //   console.log(error);
     });
 }, 1000);
